c47c5ec53c64bb6aaf9e60a0c7030079e4ab4f4b
Lucas Antunes <lucasba8@gmail.com>
2021-09-14 16:46:41 -0300

    simplify logging

--- main2.go
+++ main2.go
@@ -2,15 +2,9 @@ package main
 
 import (
 	"fmt"
-	"sort"
 	"sync"
 	"time"
 )
-
-type Action struct {
-	name      string
-	timestamp time.Time
-}
 
 const PHILOSOPHER_COUNT = 5
 const ITERATIONS = 10
@@ -18,17 +12,12 @@ const ITERATIONS = 10
 var forkMutexes [PHILOSOPHER_COUNT]sync.Mutex
 var grabbingMutex sync.Mutex
 
-var actions [][]Action
-
 func main() {
 	startTime := time.Now()
-	actions = make([][]Action, PHILOSOPHER_COUNT)
 
 	var waitGroup sync.WaitGroup
 
 	for philId := 0; philId < PHILOSOPHER_COUNT; philId++ {
-		actions[philId] = make([]Action, 0)
-
 		waitGroup.Add(1)
 		go runPhisolopher(philId, &waitGroup)
 	}
@@ -38,15 +27,13 @@ func main() {
 	endTime := time.Now()
 
 	fmt.Printf("Duration: %f\n", endTime.Sub(startTime).Seconds())
-
-	printTimings(endTime.Sub(startTime))
 }
 
 func runPhisolopher(philId int, waitGroup *sync.WaitGroup) {
 	defer waitGroup.Done()
 
 	for i := 0; i < ITERATIONS; i++ {
-		actions[philId] = append(actions[philId], Action{name: "Thinking", timestamp: time.Now()})
+		LogAction(philId, "Thinking")
 		think()
 
 		leftFork := &forkMutexes[leftForkId(philId)]
@@ -57,15 +44,18 @@ func runPhisolopher(philId int, waitGroup *sync.WaitGroup) {
 		// wants a fork that's being used by someone else all other philosophers must
 		// wait for that philosofer to get his fork in order to be able to get theirs,
 		// even if they are different forks.
-		actions[philId] = append(actions[philId], Action{name: "Waiting(G)", timestamp: time.Now()})
+		LogAction(philId, "Waiting(G)")
 		grabbingMutex.Lock()
-		actions[philId] = append(actions[philId], Action{name: "Waiting(L)", timestamp: time.Now()})
+
+		LogAction(philId, "Waiting(L)")
 		leftFork.Lock()
-		actions[philId] = append(actions[philId], Action{name: "Waiting(R)", timestamp: time.Now()})
+
+		LogAction(philId, "Waiting(R)")
 		rightFork.Lock()
+
 		grabbingMutex.Unlock()
 
-		actions[philId] = append(actions[philId], Action{name: "Eating", timestamp: time.Now()})
+		LogAction(philId, "Eating")
 		eat()
 
 		leftFork.Unlock()
@@ -93,56 +83,4 @@ func rightForkId(philId int) int {
 	}
 
 	return forkId
-}
-
-type IdentifiedAction struct {
-	Action
-	philId int
-}
-
-type ByTimestamp []IdentifiedAction
-
-func (a ByTimestamp) Len() int      { return len(a) }
-func (a ByTimestamp) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
-func (a ByTimestamp) Less(i, j int) bool {
-	return a[i].timestamp.Nanosecond() < a[j].timestamp.Nanosecond()
-}
-
-func printTimings(duration time.Duration) {
-	var allActions []IdentifiedAction = make([]IdentifiedAction, 0)
-	for philId := 0; philId < PHILOSOPHER_COUNT; philId++ {
-		for _, action := range actions[philId] {
-			allActions = append(allActions, IdentifiedAction{
-				philId: philId,
-				Action: action,
-			})
-		}
-	}
-
-	sort.Sort(ByTimestamp(allActions))
-
-	colWidth := 15
-
-	for i := 0; i < PHILOSOPHER_COUNT; i++ {
-		fmt.Printf("\033[%dC|", colWidth-1)
-	}
-
-	for i := 0; i < PHILOSOPHER_COUNT; i++ {
-		fmt.Printf("\033[1G\033[%dC%d", i*colWidth, i+1)
-	}
-
-	print("\n")
-
-	for _, action := range allActions {
-		tabCount := action.philId
-
-		print("\033[1G")
-		for i := 0; i < PHILOSOPHER_COUNT; i++ {
-			fmt.Printf("\033[%dC|", colWidth-1)
-		}
-
-		print("\033[1G")
-		fmt.Printf("\033[%dC %s\n", tabCount*colWidth, action.name)
-	}
-
 }
